ad891939b9a7beb41d6ea28ddd0d5fd24da0a65c
amandeep <amanchauhan813@gmail.com>
2024-04-14 13:44:29 +0100

    Fizz_Buzz Game

---
+++ Fizz_Buzz.py
@@ -0,0 +1,17 @@
+#write a code in python in which you can get “Fizz Buzz” for 
+#all numbers which can be divided by (3, 5, 15).
+#The range should from (1 to 100).
+
+#class Fizz:
+ #   def __init__(self) -> None:
+  #      pass
+
+for i in range(1,100):
+    if i%3==0:
+        print('Fizz')
+    elif i%5==0:
+        print('Buzz')
+    elif i%15==0:
+        print('FizzBuzz')
+    else:
+        print(i)
